f89e75a90a06a87bed0693f1069a8f08506b64dc
Peter Ort <peter@peterort.com>
2020-11-11 14:21:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,11 +7,11 @@ class TicTacToe
 
 
     def display_board
-      print "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+      print " #{@board[0]} | #{@board[1]} | #{@board[2]} "
       print "---------"
-      print "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+      print " #{@board[3]} | #{@board[4]} | #{@board[5]} "
       print "---------"
-      print "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+      print " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
 
 
