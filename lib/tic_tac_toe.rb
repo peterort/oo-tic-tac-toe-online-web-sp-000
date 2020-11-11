9f8df794d9ac2d09698b61355e37415ed8fa5ca8
Peter Ort <peter@peterort.com>
2020-11-11 14:52:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,7 @@ class TicTacToe
     end
 
     def valid_move?(input)
-      if position_taken?(input) == false && valiid_move?(input) <= 8
+      if position_taken?(input) == false && valid_move?(input) <= 8
         return true
       end
     end
