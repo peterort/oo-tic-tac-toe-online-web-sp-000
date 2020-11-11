d2a7fcd840f7b653e42d9d4ee551a272df5458c3
Peter Ort <peter@peterort.com>
2020-11-11 14:06:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
-  def WIN_COMBINATIONS
+  WIN_COMBINATIONS[
     [0,1,2]
     [3,4,5]
     [6,7,8]
@@ -11,7 +11,8 @@ class TicTacToe
     [2,5,8]
     [0,4,8]
     [2,4,6]
-  end
+  ]
+
 end
 
 
